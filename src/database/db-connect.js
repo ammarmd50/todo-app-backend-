40a1606cc242f30ef27bd7f93774da4410250060
Md Ammar <ammarmd50@gmail.com>
2026-01-04 21:11:17 +0530

    fix build, add mongo uri

--- src/database/db-connect.js
+++ src/database/db-connect.js
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 // Connect MongoDB
 export const connectDb = async () => {
-
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/mydb")
-        console.log("MongoDB connected")
-
-    } catch (error) {
-        console.log(err)
-
-    }
-
-}
+  const mongoUsername = "ammarmd50";
+  const mongoPassword = "pcS5YKiQJMa0O3Ep";
+  const dbname = "mydb";
+  const mongoUri = `mongodb+srv://${mongoUsername}:${mongoPassword}@cluster0.iuyjrlw.mongodb.net/${dbname}?appName=Cluster0`;
+  try {
+    await mongoose.connect(mongoUri);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
